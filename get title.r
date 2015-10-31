206e3729e7c1f147e564278b2af10a802f4e92c5
XavierXiao <xiaozhisheng950@gmail.com>
2015-10-30 23:04:17 -0700

    Update get title.r

--- get title.r
+++ get title.r
@@ -8,6 +8,7 @@ ns2=sapply(ns, xmlValue)
 
 class(ns2)
 
+#title
 temp_title=rep(0,length(ns2))
 
 for (i in 1:length(ns2)){
@@ -21,7 +22,7 @@ for (i in 1:length(ns2)){
 
 r=which(temp_title %in% 1)
 title=ns2[r]
-
+title
 for (i in 1:length(title)){
   t=title[i]
   t=substr(t, 4, nchar(t))
@@ -30,3 +31,43 @@ for (i in 1:length(title)){
   title[i]=t
 }
 DATA=data.frame('title'=title)
+
+#citation
+temp.cite=rep(0,length(ns2))
+for (i in 1:length(ns2)){
+  if (!is.null(ns2[i])){
+    if (grepl("Z9 ", ns2[i])){
+      temp.cite[i]=1
+    }
+  }
+}
+r=which(temp.cite %in% 1)
+cite=ns2[r]
+for (i in 1:length(cite)){
+  t=cite[i]
+  t=substr(t, 4, nchar(t))
+  t=gsub('\n','',t)
+  cite[i]=t
+}
+cite=as.integer(cite)
+DATA=data.frame(DATA,'citation'=cite)
+
+#Author
+temp.au=rep(0,length(ns2))
+for (i in 1:length(ns2)){
+  if (!is.null(ns2[i])){
+    if (grepl("AU ", ns2[i])){
+      temp.au[i]=1
+    }
+  }
+}
+r=which(temp.au %in% 1)
+au=ns2[r]
+for (i in 1:length(au)){
+  t=au[i]
+  t=substr(t, 4, nchar(t))
+  t=gsub('\n','',t)
+  t=gsub('  ',';',t)
+  au[i]=t
+}
+DATA=data.frame(DATA,'author'=au)
