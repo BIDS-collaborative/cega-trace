9852266186b5456d8435b4c83b7e5222ea47d53a
XavierXiao <xiaozhisheng950@gmail.com>
2015-10-30 22:18:32 -0700

    Create get title.r

---
+++ get title.r
@@ -0,0 +1,33 @@
+library(XML)
+a = htmlTreeParse(file.choose(),useInternal = TRUE)
+
+xmlValue(a) 
+
+ns <- getNodeSet(a, '//tr')
+
+ns2=sapply(ns, xmlValue)
+
+class(ns2)
+
+temp_title=rep(0,length(ns2))
+
+for (i in 1:length(ns2)){
+  if (!is.null(ns2[i])){
+    if (grepl("TI ", ns2[i])){
+      temp_title[i]=1
+    }
+  }
+  
+}
+
+r=which(temp_title %in% 1)
+title=ns2[r]
+title
+for (i in 1:length(title)){
+  t=title[i]
+  t=substr(t, 4, nchar(t))
+  t=gsub('\n','',t)
+  
+  title[i]=t
+}
+DATA=data.frame('title'=title)
