9998be3728d953b0482a5dafc77bee8743e8c0bd
manjiangjie <812396231@qq.com>
2015-12-02 17:16:38 -0800

    update google scholar API

--- sample_parce.py
+++ sample_parce.py
@@ -1,43 +1,19 @@
 import csv
 import os
 import re
+import codecs
+import scholar
 
 
 current_path = os.getcwd() + "/output.csv"
-file_name = 'parce_sample.txt'
 csv_columns = ['name', 'date', 'article']
-par_file = "paragraph_parce.txt"
+parse_file = "target.txt"
 
 
-def parseTxtFile(file_name):
-	citation = []
-	with open(file_name, 'r') as txt_file:
-		txt = txt_file.readlines()
-	for line in txt:
-		strings = line.split('.')
-		citation.append(dict([("name", strings[0]), ("date", int(strings[1][1:])), ("article", strings[2][2:])]))
-	return citation
-
-def writeDictToCSV(csv_file, csv_columns, dict_data):
-    with open(csv_file, 'w') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames = csv_columns)
-        writer.writeheader()
-        for data in dict_data:
-            writer.writerow(data)
-    return 
-
-def parseLeftAndRight(par_file):
-	left,right = [],[]
-	with open(par_file) as f:
-		data = f.readlines()
-	for line in data:
-		index = line.find('  ')
-		if index == -1:
-			left.append(line)
-		else:
-			left.append(line[:index])
-			right.append(line[index:])
-	return left, right
+def decode(parse_file):
+    with codecs.open(parse_file, 'r+', encoding='IBM855') as txt_file:
+        txt = txt_file.readlines()
+    return txt
 
 def concate(lines):
     str1 = ""
@@ -51,14 +27,90 @@ def concate(lines):
             str1 = ''
     return L
 
+def concate2(lines):
+    str1 = ""
+    L = []
+    for line in lines:
+        if line[-3] != ".":
+            line = line.strip("\n")
+            line = line.strip("\r")
+            str1 += line
+        else:
+            str1 += " " + line
+            str1 = str1.strip("\n")
+            str1 = str1.strip("\r")
+            L.append(str1)
+            str1 = ''
+    return L
 
-left, right = parseLeftAndRight(par_file)
+def parseTxtFile(lines):
+    parse_txt = []
+
+    for line in lines:
+        line = line.lstrip()
+        if not (len(line) <= 5 or (line[0] != " " and line[1] == " ") or line[0] == "\n"):
+            parse_txt.append(line)
+
+    l = concate2(parse_txt)
+    l[0] = l[0][10:]
+    # print(l[0])
+
+    return l
+
+    # txt = open("parce_sample.txt", "w")
+    # for a in l:
+    #     txt.write(a)
+    # txt.close()
+
+    # citation = []
+    # for line in l:
+    #     strings = line.split('.')
+    #     citation.append(dict([("name", strings[0]), ("date", int(strings[1][1:])), ("article", strings[2][2:])]))
+    # return citation
+
+def writeDictToCSV(csv_file, csv_columns, dict_data):
+    with open(csv_file, 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames = csv_columns)
+        writer.writeheader()
+        for data in dict_data:
+            writer.writerow(data)
+    return dict_data
+
+def parseLeftAndRight(txt):
+	left,right = [], []
+	for line in txt:
+		index = line.find('  ')
+		if index == -1:
+			left.append(line)
+		else:
+			left.append(line[:index])
+			right.append(line[index:])
+	return left, right
+
+
+'''
+left, right = parseLeftAndRight(decode(parse_file))
 l = concate(left)
 
 txt = open("parce_sample.txt", "w")
 for a in l:
     txt.write(a[1:] + "\n")
 txt.close()
+'''
 
-writeDictToCSV(current_path, csv_columns, parseTxtFile(file_name))
+sample = decode(parse_file)
+citation = parseTxtFile(sample)
 
+# dict_data = writeDictToCSV(current_path, csv_columns, citation)
+# print(dict_data)
+
+querier = scholar.ScholarQuerier()
+settings = scholar.ScholarSettings()
+query = scholar.SearchScholarQuery()
+querier.apply_settings(settings)
+
+for c in citation:
+    print(c)
+    query.set_words(c)
+    querier.send_query(query)
+    scholar.csv(querier, header=True, sep='|')
